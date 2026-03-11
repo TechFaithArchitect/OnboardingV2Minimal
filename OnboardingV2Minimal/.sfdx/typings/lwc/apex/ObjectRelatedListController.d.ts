a85de3cd5d8f9d9748bf8c306c43b7c9a119f9af
Jason Murray <scott.murray@digitaltraffic.com>
2026-03-11 10:35:57 -0400

    Consolidated Program Dates selectors into ObjectRelatedListController.

---
+++ OnboardingV2Minimal/.sfdx/typings/lwc/apex/ObjectRelatedListController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/ObjectRelatedListController.getRelatedRecords" {
+  export default function getRelatedRecords(param: {config: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ObjectRelatedListController.getLookupOptions" {
+  export default function getLookupOptions(param: {objectApiName: any, labelFieldApiName: any, orderByField: any, orderDirection: any, recordLimit: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ObjectRelatedListController.getProgramDates" {
+  export default function getProgramDates(param: {accountId: any}): Promise<any>;
+}
